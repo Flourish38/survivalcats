b8d2228cb824da3184ae638e77d7124ab0363c8d
Flourish38 <emisandvik@gmail.com>
2024-12-06 14:38:20 -0500

    add old code copied from https://github.com/ZigTheHedge/vsmods/tree/cf8748e08739b248725b2a316e346fb19e3907f1

---
+++ old_code/survivalcats/src/GuiHandbookBookmarkedItemStackPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace survivalcats.src
+{
+    class GuiHandbookBookmarkedItemStackPage : GuiHandbookItemStackPage
+    {
+        public override string CategoryCode => "bookmark";
+
+        public GuiHandbookBookmarkedItemStackPage(ICoreClientAPI capi, ItemStack stack) : base(capi, stack)
+        {
+            Visible = true;
+        }
+    }
+}
---
+++ old_code/survivalcats/src/SurvivalCategories.cs
@@ -0,0 +1,49 @@
+﻿using Foundation.Extensions;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace survivalcats.src
+{
+    public class ModConfigFile
+    {
+        public static ModConfigFile Current { get; set; }
+
+        public bool addModNameToInfo { get; set; } = true;
+        public bool displayVanillaTabsAsCategories { get; set; } = true;
+        public bool displayModsAsCategories { get; set; } = true;
+    }
+
+    class SurvivalCategories : ModSystem
+    {
+        Harmony harmony = new Harmony("com.cwelth.survivalcats");
+
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.Start(api);
+
+            ModConfigFile.Current = api.LoadOrCreateConfig<ModConfigFile>("SurvivalCatsConfig.json");
+
+            //Harmony.DEBUG = true;
+            
+            harmony.PatchAll();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            harmony.UnpatchAll("com.cwelth.survivalcats");
+        }
+    }
+}
